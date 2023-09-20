593a7b775c1eaf4196d4c7f29d8e832a5a1b2398
lavanyadasari <lavanya.dasari@everest.engineering>
2023-09-20 23:14:31 +0530

    Basic text has been created

--- App.tsx
+++ App.tsx
@@ -1,1 +1,16 @@
+import React, {useEffect} from 'react';
+import {View, SafeAreaView, Text} from 'react-native/types';
 
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          Hello
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
