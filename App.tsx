734c81313163e081085691fe3aa547511f188e4c
lavanyadasari <lavanya.dasari@everest.engineering>
2023-09-20 23:17:51 +0530

    Users details are stored in static data

--- App.tsx
+++ App.tsx
@@ -3,6 +3,53 @@ import {View, SafeAreaView, Text} from 'react-native';
 
 const imageUrl = 'https://static.thenounproject.com/png/3137368-200.png';
 
+export type details = {
+  name: string;
+  address: string;
+  id: number;
+  img: string;
+};
+
+
+const staticData: details[] = [
+  {
+    name: 'Kathryn Murphy',
+    address: '6391 Elgin St. Celina, Delaware',
+    img: 'https://user-images.githubusercontent.com/1954752/120525494-2b195500-c3a6-11eb-9b00-ad81434be18d.png',
+    id: 1,
+  },
+  {
+    name: 'Eleanor Pena',
+    address: '2464 Royal Ln. Mesa, New Jersey 454663',
+    img: 'https://raw.githubusercontent.com/tom555my/react-native-avataaars/HEAD/avataaars.png',
+    id: 2,
+  },
+  {
+    name: 'Marvin McKinney',
+    address: '2715 Ash Dr. San Jose, South Dakota 83475',
+    img: 'https://raw.githubusercontent.com/Shiroi-shi/avataaars/HEAD/avataaars-example.png?raw=true',
+    id: 3,
+  },
+  {
+    name: 'Ronald Richards',
+    address: '3891 Ranchview Dr. Richardson, California 62639',
+    img: 'https://pbs.twimg.com/media/DPFz5pjWsAA9bjE.png',
+    id: 4,
+  },
+  {
+    name: 'Theresa Webb',
+    address: '2118 Thornidge Cir. Syracuse, Connecticut 35624',
+    img: 'https://koolinus.files.wordpress.com/2019/03/avataaars-e28093-koolinus-1-12mar2019.png',
+    id: 5,
+  },
+  {
+    name: 'Floyd Miles',
+    address: '1901 Thornridge Cir. Shiloh, Hawaii 81063',
+    img: 'https://raw.githubusercontent.com/lek890/avataaars/HEAD/avataaars-example-pic.png?raw=true',
+    id: 6,
+  },
+];
+
 const App = () => {
   return (
     <SafeAreaView>
