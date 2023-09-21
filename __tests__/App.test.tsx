bd6129471479a18b88b412395004675067050455
lavanyadasari <lavanya.dasari@everest.engineering>
2023-09-21 14:53:34 +0530

    Tests for rendering contact list

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,9 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
+import {render, screen} from '@testing-library/react-native';
 import App from '../App';
 
-// Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders contact list', () => {
+  render(<App />);
+  const header = screen.getByText('Contact List');
+  expect(header).toBeTruthy();
 });
